1df3d5bb3dbfdc4821740ad6e3fe9268cfb1564f
Temitope Akinsoto <takinsoto@gmail.com>
2020-02-14 01:44:56 +0100

    write list comprehension that creates a list of tuples containing name and age e.g (David, 32)

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -54,7 +54,7 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(h.name, h.age) for h in humans if 27 <= h.age <= 32]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
